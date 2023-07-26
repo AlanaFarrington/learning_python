1a05e68a63abf816c2506e606a80a3e59bf9537f
AlanaFarrington <140013036+AlanaFarrington@users.noreply.github.com>
2023-07-26 22:37:37 +0100

    New feature module (#1)
    * implemented add functionality
    
    * added subtract functionality
    
    * updated readme
    
    * new divide file
    
    * new commit
    
    * added multiply functionality

---
+++ multiply.py
@@ -0,0 +1,5 @@
+def mutliply_two_numbers(a, b):
+    return a * b
+
+def mutliply_three_numbers(a, b, c):
+    return a * b * c 
---
+++ subtract.py
@@ -0,0 +1,5 @@
+def subtract_two_numbers(a, b):
+    return a - b
+
+def subtract_three_numbers(a, b, c):
+    return a - b - c 
